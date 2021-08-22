d523621e67acbfdfa6e48ecb0edd2dba6a1d51a8
Seongyong Park <EuphoricCatface@gmail.com>
2021-08-22 22:00:57 +0900

    Convert vid_gen to generalized hvsync_gen

--- fpga4fun_pong_vivado.srcs/sources_1/new/vid_gen.v
+++ fpga4fun_pong_vivado.srcs/sources_1/new/vid_gen.v
@@ -20,29 +20,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module vid_gen(
+module hvsync_gen(
     input CLK,
     input RST,
 
-    output VGA_RED_3,
-    output VGA_BLUE_3,
-    output VGA_GREEN_3,
+    output inDisplayArea,
+    output[10:0] CounterX,
+    output[10:0] CounterY,
 
     output vga_h_sync,
     output vga_v_sync
 );
     // Timings are based on http://tinyvga.com/vga-timing/1280x1024@60Hz
     // The devices I have for testing do not support 640x480, 720x576 or 800x600 resolutions.
-    reg[10:0] CounterX;
-    reg[10:0] CounterY;
     wire CounterXmaxed = (CounterX==11'h697);
     wire CounterYmaxed = (CounterY==11'h429);
+    reg[10:0] CounterX;
+    reg[10:0] CounterY;
 
     always @(posedge CLK)
     if (RST)
         begin
             CounterX <= 0;
             CounterY <= 0;
+            inDisplayArea <= 0;
         end
     else
         if(CounterXmaxed)
@@ -65,11 +66,15 @@ module vid_gen(
         vga_HS <= (CounterX[10:6] == 5'h34); // active for 64 clocks
         vga_VS <= (CounterY[10:2] == 9'h10a); // active for 4 lines
     end
+
+    reg inDisplayArea;
+    always @(posedge CLK)
+    if (inDisplayArea == 0)
+        inDisplayArea <= (CounterXmaxed) && (CounterY < 1024);
+    else
+        inDisplayArea <= !(CounterX == 1279);
+
     assign vga_h_sync = ~vga_HS;
     assign vga_v_sync = ~vga_VS;
-
-    assign VGA_RED_3 = CounterY[3] | (CounterX == 256);
-    assign VGA_GREEN_3 = (CounterX[5] ^ CounterX[6]) | (CounterX == 256);
-    assign VGA_BLUE_3 = CounterX[4] | (CounterX == 256);
 
 endmodule
